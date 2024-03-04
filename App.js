a3193b4bca83fad819c41d4ca420c2cdf130f00e
Shefali1499 <shefalisawant1499@gmail.com>
2024-03-04 22:36:59 +0530

    package json added,React createElement vs Jsx

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+//React.createElement=>ReactElemet-JS Object=> HTMLElement(render)
 const heading=React.createElement("h1",{
     id:"heading"
-},"Hello world react");
-    const root=ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+},"Hello world react 😀");
+const newHeading=<h1 className="heading">Hello world react JSX😀</h1>
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(newHeading);
